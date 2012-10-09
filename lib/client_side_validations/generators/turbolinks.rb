c827b346ade1a2771d69c1ce9240329c1fa97e0d
Brian Cardarella <bcardarella@gmail.com>
2012-10-09 13:06:41 -0400

    Revert "version bump for final release"
    This reverts commit 5976fe04aaac9f7d0baa58390892d6d28c854aa8.

---
+++ lib/client_side_validations/generators/turbolinks.rb
@@ -0,0 +1,17 @@
+module ClientSideValidations
+  module Generators
+    class Turbolinks
+      def self.assets
+        [{
+          :path => File.expand_path('../../../../vendor/assets/javascripts', __FILE__),
+          :file => 'rails.validations.turbolinks.js'
+        }]
+      end
+
+      Generators.register_assets(self)
+    end
+  end
+end
+
+
+
--- lib/client_side_validations/turbolinks/version.rb
+++ lib/client_side_validations/turbolinks/version.rb
@@ -1,5 +1,5 @@
 module ClientSideValidations
   module Turbolinks 
-    VERSION = '0.5.0'
+    VERSION = '0.5.0.rc.1'
   end
 end
